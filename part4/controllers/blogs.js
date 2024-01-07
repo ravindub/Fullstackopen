d0fe643c8919d4fc5e822512b00e006a960cd222
ravindub <ravindubandara075@gmail.com>
2024-01-07 13:18:03 +0200

    complete ex 4.22

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,6 @@
-const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
-const User = require('../models/user');
+const middleware = require('../utils/middleware');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { blogs: 0 });
@@ -19,71 +18,59 @@ blogsRouter.get('/:id', async (request, response, next) => {
   }
 });
 
-blogsRouter.post('/', async (request, response, next) => {
-  const body = request.body;
+blogsRouter.post(
+  '/',
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    const user = request.user;
 
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' });
+    const blog = new Blog(body);
+
+    blog.user = user._id;
+
+    if (body.likes === undefined) {
+      blog.likes = 0;
+    }
+
+    if (!body.title || !body.url) {
+      return response.status(400).json({
+        error: 'Title or Url cannot be empty'
+      });
+    }
+
+    const savedBlog = await blog.save();
+
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    response.status(201).json(savedBlog);
   }
+);
 
-  const user = await User.findById(decodedToken.id);
+blogsRouter.delete(
+  '/:id',
+  middleware.userExtractor,
+  async (request, response) => {
+    const user = request.user;
 
-  if (!user) {
-    return response.status(404).end();
+    const blog = await Blog.findById(request.params.id);
+
+    if (!blog) {
+      return response.status(404).end();
+    }
+
+    if (blog.user.toString() === user._id.toString()) {
+      await Blog.findByIdAndDelete(request.params.id);
+      response.status(204).end();
+    } else {
+      return response.status(401).json({ error: 'invalid user' });
+    }
   }
+);
 
-  const blog = new Blog(body);
-
-  blog.user = user._id;
-
-  if (body.likes === undefined) {
-    blog.likes = 0;
-  }
-
-  if (!body.title || !body.url) {
-    return response.status(400).json({
-      error: 'Title or Url cannot be empty'
-    });
-  }
-
-  const savedBlog = await blog.save();
-
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
-
-  response.status(201).json(savedBlog);
-});
-
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' });
-  }
-
-  const user = await User.findById(decodedToken.id);
-
-  if (!user) {
-    return response.status(404).end();
-  }
-
-  const blog = await Blog.findById(request.params.id);
-
-  if (!blog) {
-    return response.status(404).end();
-  }
-
-  if (blog.user.toString() === user._id.toString()) {
-    await Blog.findByIdAndDelete(request.params.id);
-    response.status(204).end();
-  } else {
-    return response.status(401).json({ error: 'invalid user' });
-  }
-});
-
-blogsRouter.put('/:id', async (request, response) => {
+blogsRouter.put('/:id', middleware.userExtractor, async (request, response) => {
   const blog = {
     likes: request.body.likes
   };
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization');
@@ -8,6 +10,24 @@ const tokenExtractor = (request, response, next) => {
   } else {
     request.token = null;
   }
+
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  if (!user) {
+    return response.status(404).end();
+  }
+
+  request.user = user;
 
   next();
 };
@@ -46,6 +66,7 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   tokenExtractor,
+  userExtractor,
   requestLogger,
   unknownEndpoint,
   errorHandler
